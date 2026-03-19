a8dd5e08f927f581c2b204f8887f6de40a0a4cd8
Derrick Marfo <steammarfo111@gmail.com>
2026-03-20 00:01:48 +0100

    first phase of chunking complete

--- RAG/chunking.py
+++ RAG/chunking.py
@@ -11,6 +11,32 @@ def chunk_text(text:str, chunk_size:int, use_sentences:bool=True, overlap:int=50
     text=text.strip()
     if not text:
         return[]
+    sentences=nltk.sent_tokenize(text)
+    all_tokens=[]
+    sentence_map=[]
     
+    for i, sentences in enumerate(sentences):
+        tokens=encoding.encode(sentences)
+        all_tokens.extend(tokens)
+        sentence_map.extend([i]*len(tokens))
+        
+        chunks=[]
+        start =0
+        total_tokens=len(all_tokens)
+    
+    while start<total_tokens:
+        end=min(start+chunk_size, total_tokens)
+        chunk_tokens=all_tokens[start:end]
+        chunk_text_str=encoding.decode(chunk_tokens)
+        
+        chunks.append({
+            "chunk":chunk_text_str,
+            "start_token":start,
+            "end_token":end,
+        })
+        start += chunk_size - overlap
+            
+    return chunks
+      
     
     
